ed465887810d1af69f399b8a6b6b1886a6f44dd7
Alireza Dehghan <ar.dehghan.a@gmail.com>
2024-08-09 16:48:52 +0330

    refactor: Enhance useChromeLocalStorage hook performance for faster local storage access and better resource utilization

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,41 +1,44 @@
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 
 function useChromeLocalStorage<T>(
   key: string,
-  initialValue: T
-): [
-  value: T,
-  setLocalStorage: (newValue: T) => void,
-  clearLocalStorage: () => void
-] {
+  initialValue: T,
+  sync = false
+): [T, (newValue: T | ((prevValue: T) => T)) => void, () => void] {
   const [value, setValue] = useState<T>(initialValue);
 
   useEffect(() => {
     chrome.storage.local.get([key], result => {
-      if (result[key]) setValue(result[key]);
+      if (result[key]) setValue(result[key] as T);
     });
 
-    const listener = (
-      changes: {[key: string]: chrome.storage.StorageChange},
-      areaName: string
-    ) => {
-      if (areaName === 'local')
-        for (const changedKey in changes)
-          if (changedKey === key) setValue(changes[changedKey].newValue);
-    };
+    if (sync) {
+      const listener = (
+        changes: {[key: string]: chrome.storage.StorageChange},
+        areaName: string
+      ) => {
+        if (areaName === 'local' && changes[key]) {
+          setValue(changes[key].newValue as T);
+        }
+      };
+      chrome.storage.onChanged.addListener(listener);
+      return () => chrome.storage.onChanged.removeListener(listener);
+    }
+  }, [key, sync]);
 
-    chrome.storage.onChanged.addListener(listener);
-    return () => chrome.storage.onChanged.removeListener(listener);
-  }, [key]);
+  const setLocalStorage = useCallback(
+    (newValue: T | ((prevValue: T) => T)) => {
+      const valueToStore = newValue instanceof Function ? newValue(value) : newValue;
+      setValue(valueToStore);
+      chrome.storage.local.set({[key]: valueToStore});
+    },
+    [key, value]
+  );
 
-  const setLocalStorage = (newValue: any) => {
-    setValue(newValue);
-    chrome.storage.local.set({[key]: newValue});
-  };
-
-  const clearLocalStorage = () => {
+  const clearLocalStorage = useCallback(() => {
+    setValue(initialValue);
     chrome.storage.local.remove(key);
-  };
+  }, [key, initialValue]);
 
   return [value, setLocalStorage, clearLocalStorage];
 }
